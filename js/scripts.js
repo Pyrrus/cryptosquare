8e4403c81e56733c106ec427350b0a0ecd4e99e3
Adam Gorbahn and Ben Furdui <ben@>
2016-08-18 13:04:31 -0700

    V1 Cryptosquare

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// front
+$(document).ready(function () {
+  $("button").click(function () {
+    var word = $("#word").val();
+    var output = Encryption(word);
+    $("#output").text(output);
+  });
+});
+
+
+// back
+var Encryption = function(word) {
+  var str = word.toLowerCase();
+  var len = Math.floor(Math.sqrt(str.length));
+  var output = '';
+  for (var j = 0; j < len; j++) {
+    for (var i = j; i < str.length; i += len) {
+      output +=(str.charAt(i));
+    }
+  }
+
+  return output;
+}
